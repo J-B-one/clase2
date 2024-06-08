c59077f2abac0384dd688e255876ea6e44d29c1e
Jonathan <algo@correo.cl>
2024-06-08 12:45:17 -0400

    Arreglos o listas

---
+++ clase.py
@@ -0,0 +1,7 @@
+#Arreglos (Lista)
+numero = [1,2,3,4,5]
+#Imprimir
+print("Lista de númeos", numero)
+#imprimir números individuales
+print("Lista de númeos", numero[2])
+print("Lista de númeos", numero[-1])
